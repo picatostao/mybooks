946b1ac428f87cd0d9bcf55d21d38a4a4f8e99ba
picatostao <valver123@gmail.com>
2023-06-26 20:30:53 +0200

    fin

--- src/app/shared/user.service.ts
+++ src/app/shared/user.service.ts
@@ -7,19 +7,19 @@ import { User } from '../models/user';
   providedIn: 'root'
 })
 export class UserService {
-  private url:string='http://localhost:4000'
+  private url:string='http://localhost:3000'
   public logueado:boolean=false
   public user:User|undefined;
   logged: any;
   constructor(private http:HttpClient) { 
   }
-  register(user:User):Observable<any>{
+  register(user:User){
     return this.http.post(`${this.url}/register`,user)
   }
-  login(user:User):Observable<any>{
+  login(user:User){
     return this.http.post(this.url+"/login",user)
   }
-  update(user:User):Observable<any>{
+  update(user:User){
     return this.http.put(this.url+'/update',user)
   }
 }
